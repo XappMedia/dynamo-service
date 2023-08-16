257bbbf273f8473b885da62356673341d7cf3eac
chrsdietz <chris@xappmedia.com>
2023-08-16 16:40:00 -0400

    Adding tests for update body

--- src/main/service/DynamoService/utils/UpdateBodyUtils.ts
+++ src/main/service/DynamoService/utils/UpdateBodyUtils.ts
@@ -1,5 +1,6 @@
 import { StringKeys } from "../../../types/StringKeys";
-import { Set, UpdateBody } from "../IDynamoService";
+import { objHasAttrs } from "../../../utils/Object";
+import { DynamoDBExpressionAttributeNames, Set, UpdateBody, UpdateParameters } from "../IDynamoService";
 
 
 /**
@@ -23,4 +24,121 @@ export function transferUndefinedToRemove<T>(body: UpdateBody<T>): UpdateBody<T>
         }
     }
     return { ...body, set, remove } as UpdateBody<T>;
+}
+
+
+/**
+ * A Utility method that will retrieve an UpdateBody and convert to the
+ * Expression attributes for a DynamoDB update.
+ *
+ * You can just apply the object like so:
+ *
+ * const parameters = getUpdateParameters(body);
+ * const params: DynamoDB.DocumentClient.UpdateItemInput = {
+ *    ...parameters
+ * }
+ *
+ * @param body The body to include.
+ * @return An object that contains the attributes:
+ *      {
+ *          UpdateExpression: items to update.
+ *          ExpressionAttributeValues:  The values of such items.
+ *          ExpressionAttributeNames: The names that are mapped to those expressions.
+ *      }
+ */
+export function getUpdateParameters<T>(body: UpdateBody<T>): UpdateParameters {
+    let setValues: { [key: string]: any };
+    let setAliasMap: { [key: string]: string };
+    let setExpression: string;
+    const { set, append, remove, prepend } = body;
+    if (objHasAttrs(set)) {
+        setValues = {};
+        setAliasMap = {};
+        setExpression = "set ";
+        let index = 0;
+        for (const key in set) {
+            if (set.hasOwnProperty(key)) {
+                const splitKeys = key.split(".");
+                const aliases: string[] = [];
+                for (const splitKey of splitKeys) {
+                    const matchKey = splitKey.match(/^([^[\]]+)(\[[0-9]+\])?$/);
+                    const alias = "#__dynoservice_updateset_a" + ++index + (matchKey[2] || "");
+                    setAliasMap[alias] = matchKey[1];
+                    aliases.push(alias);
+                }
+                const name = ":__dynoservice_updateset_a" + ++index;
+                setExpression += aliases.join(".") + " = " + name + ",";
+                setValues[name] = set[key as StringKeys<T>];
+            }
+        }
+    }
+
+    if (objHasAttrs(append)) {
+        setValues = setValues || {};
+        setAliasMap = setAliasMap || {};
+        setExpression = setExpression || "set ";
+        let index = 0;
+        for (const key in append) {
+            if (append.hasOwnProperty(key)) {
+                const alias = "#__dynoservice_updateappend_c" + index;
+                const name = ":__dynoservice_updateappend_c" + ++index;
+                setExpression += alias + " = list_append(if_not_exists(" + alias + ", :__dynoservice_update_append_empty_list)," + name + "),";
+                setValues[name] = append[key];
+                setValues[":__dynoservice_update_append_empty_list"] = [];
+                setAliasMap[alias] = key;
+            }
+        }
+    }
+
+    if (objHasAttrs(prepend)) {
+        setValues = setValues || {};
+        setAliasMap = setAliasMap || {};
+        setExpression = setExpression || "set ";
+        let index = 0;
+        for (const key in prepend) {
+            if (prepend.hasOwnProperty(key)) {
+                const alias = "#__dynoservice_prepend_c" + index;
+                const name = ":__dynoservice_prepend_c" + ++index;
+                setExpression += alias + " = list_append(" + name + ", if_not_exists(" + alias + ", :__dynoservice_update_prepend_empty_list)),";
+                setValues[name] = prepend[key];
+                setValues[":__dynoservice_update_prepend_empty_list"] = [];
+                setAliasMap[alias] = key;
+            }
+        }
+    }
+
+    if (remove && remove.length > 0) {
+        setValues = setValues || {};
+        setAliasMap = setAliasMap || {};
+        setExpression = setExpression ? setExpression.substr(0, setExpression.length - 1) + " remove " : "remove ";
+        remove.forEach((key: string, index) => {
+            const splitKeys = key.split(".");
+            const aliases = [];
+            for (const splitKey of splitKeys) {
+                const matchKey = splitKey.match(/^([^[\]]+)(\[[0-9]+\])?$/);
+                const alias = "#__dynoservice_updateremove_r" + ++index + (matchKey[2] || "");
+                setAliasMap[alias] = matchKey[1];
+                aliases.push(alias);
+            }
+            setExpression += aliases.join(".") + ",";
+        });
+    }
+
+    if (setExpression) {
+        setExpression = setExpression.slice(0, setExpression.length - 1); // Removes the last "," on the string.
+    }
+
+    let returnValue: UpdateParameters = { UpdateExpression: setExpression };
+    if (objHasAttrs(setValues)) {
+        returnValue = { ...returnValue, ...{ ExpressionAttributeValues: setValues } };
+    }
+    if (objHasAttrs(setAliasMap)) {
+        // If there are any arrays listed, then we need to remove them here.
+        const ExpressionAttributeNames = Object.keys(setAliasMap).reduce((last, currentKey) => {
+            last[currentKey.replace(/\[[0-9]+\]$/, "")] = setAliasMap[currentKey];
+            return last;
+        }, {} as DynamoDBExpressionAttributeNames);
+        returnValue = { ...returnValue, ...{ ExpressionAttributeNames } };
+    }
+    return returnValue;
 }
--- src/main/service/DynamoService/utils/__test__/UpdateBodyUtils.test.ts
+++ src/main/service/DynamoService/utils/__test__/UpdateBodyUtils.test.ts
@@ -1,6 +1,6 @@
 import * as Chai from "chai";
 import { UpdateBody } from "../../IDynamoService";
-import { transferUndefinedToRemove } from "../UpdateBodyUtils";
+import { getUpdateParameters, transferUndefinedToRemove } from "../UpdateBodyUtils";
 
 const expect = Chai.expect;
 
@@ -33,6 +33,79 @@ describe("UpdateBodyUtils", () => {
                 },
                 remove: ["param2", "param3", "param4", "param9"]
             });
+        });
+    });
+});
+
+describe.only(getUpdateParameters.name, () => {
+
+    it("Returns the update parameters for a set.", () => {
+        const updateObj: UpdateBody<any> = {
+            set: {
+                param1: "Value1",
+                param2: "Value2"
+            }
+        };
+        const updateParams = getUpdateParameters(updateObj);
+        expect(updateParams).to.deep.equal({
+            ExpressionAttributeNames: {
+                "#__dynoservice_updateset_a1": "param1",
+                "#__dynoservice_updateset_a3": "param2"
+            },
+            ExpressionAttributeValues: {
+                ":__dynoservice_updateset_a2": "Value1",
+                ":__dynoservice_updateset_a4": "Value2"
+            },
+            UpdateExpression: "set #__dynoservice_updateset_a1 = :__dynoservice_updateset_a2,#__dynoservice_updateset_a3 = :__dynoservice_updateset_a4"
+        });
+    });
+
+    it("Handles dotted parameters for set", () => {
+        const updateObj: UpdateBody<any> = {
+            set: {
+                "param1.param2": "Value1",
+                param2: "Value2"
+            }
+        };
+        const updateParams = getUpdateParameters(updateObj);
+        expect(updateParams).to.deep.equal({
+            ExpressionAttributeNames: {
+                "#__dynoservice_updateset_a1": "param1",
+                "#__dynoservice_updateset_a2": "param2",
+                "#__dynoservice_updateset_a4": "param2"
+            },
+            ExpressionAttributeValues: {
+                ":__dynoservice_updateset_a3": "Value1",
+                ":__dynoservice_updateset_a5": "Value2"
+            },
+            UpdateExpression: "set #__dynoservice_updateset_a1.#__dynoservice_updateset_a2 = :__dynoservice_updateset_a3,#__dynoservice_updateset_a4 = :__dynoservice_updateset_a5"
+        });
+    });
+
+    it("Handles append with dot", () => {
+        const updateObj: UpdateBody<any> = {
+            append: {
+                "param1.param2": "Value1",
+                param2: "Value2"
+            }
+        };
+        const updateParams = getUpdateParameters(updateObj);
+        expect(updateParams).to.deep.equal({
+            ExpressionAttributeNames: {
+                "#__dynoservice_updateappend_c0": "param1.param2",
+                "#__dynoservice_updateappend_c1": "param2"
+
+            },
+            ExpressionAttributeValues: {
+                ":__dynoservice_update_append_empty_list": [],
+                ":__dynoservice_updateappend_c1": "Value1",
+                ":__dynoservice_updateappend_c2": "Value2"
+            },
+            UpdateExpression: "set #__dynoservice_updateappend_c0 = " +
+                "list_append(if_not_exists(#__dynoservice_updateappend_c0, :__dynoservice_update_append_empty_list)," +
+                ":__dynoservice_updateappend_c1),#__dynoservice_updateappend_c1 = " +
+                "list_append(if_not_exists(#__dynoservice_updateappend_c1, :__dynoservice_update_append_empty_list)," +
+                ":__dynoservice_updateappend_c2)"
         });
     });
 });
